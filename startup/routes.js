c680e134a2b054d7b7539c70281ab6885b699ef1
Shaheer Shaikh <shayana803@gmail.com>
2021-03-27 17:00:14 +0500

    minor change

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,9 @@
  * @param {*} app express applications.
  */
 module.exports = function (app) {
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
   app.use('/api/files', require('../routes/files'));
   app.use('/api/spell', require('../routes/spell'));
 };
